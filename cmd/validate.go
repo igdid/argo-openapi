1710e78fbc92655bb7c7c5f8f6a55a35ff012f5b
Igor Diakonov <igor@linux.com>
2026-03-29 17:13:06 +0300

    Toying with reading protocols

--- cmd/validate.go
+++ cmd/validate.go
@@ -43,7 +43,34 @@ func validateOpenAPI(cmd *cobra.Command, args []string) {
 	if err := doc.Validate(ctx); err != nil {
 		log.Fatalf("invalid spec: %v", err)
 	}
-
+	if doc.Paths != nil {
+		for name, path := range doc.Paths.Map() {
+			if path.Get != nil {
+				if len(path.Get.Parameters) == 0 {
+					log.Info("GET ", name)
+				}
+				for _, p := range path.Get.Parameters {
+					log.Info("GET ", name, " ", p.Value.Name, " ", p.Value.In)
+				}
+			}
+			if path.Post != nil {
+				if len(path.Post.Parameters) == 0 {
+					log.Info("POST ", name)
+				}
+				for _, p := range path.Post.Parameters {
+					log.Info("POST ", name, p)
+				}
+			}
+			if path.Put != nil {
+				if len(path.Put.Parameters) == 0 {
+					log.Info("PUT ", name)
+				}
+				for _, p := range path.Put.Parameters {
+					log.Info("PUT ", name, p)
+				}
+			}
+		}
+	}
 	fmt.Println("OpenAPI spec is valid")
 }
 
